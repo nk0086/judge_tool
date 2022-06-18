4116e460c3dd0580d52daee608d871b7a588612a
nk86 <n172586k@gmail.com>
2022-06-18 22:24:15 +0900

    test only

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,11 @@
 mod get_testcase;
 mod submit;
 mod test_judge;
+use crate::atcoder::login;
 use crate::get_testcase::get_testcase;
 use crate::submit::submit;
 use crate::test_judge::test_judge;
+
 use anyhow::{ensure, Context, Result};
 use clap::Parser;
 use std::env;
@@ -18,18 +20,13 @@ use std::fs;
 
 fn main() -> Result<()> {
     let args = Arg::parse();
-    //let mut path = env::current_dir()?;
 
     if let Some(file_name) = args.new {
         get_testcase(&file_name)?;
     } else if let Some(file_name) = args.test {
         test_judge(&file_name)?;
-    } else if let Some(file_name) = args.submit {
-        submit(&file_name)?;
     }
-
     Ok(())
-    //println!("{:?}", args);
 }
 
 #[derive(Parser, Debug)]
@@ -37,14 +34,12 @@ fn main() -> Result<()> {
     name = "auto judge tools",
     version = "1.0.0",
     author = "nk0086",
-    about = "Support for testing and submitting code in AtCoder."
+    about = "Support for testing code in AtCoder."
 )]
 struct Arg {
     ///select option: make file, test code, submit code
     #[clap(short, long)]
     test: Option<String>,
-    #[clap(short, long)]
-    submit: Option<String>,
     #[clap(short, long)]
     new: Option<String>,
 }
--- src/submit.rs
+++ src/submit.rs
@@ -1,5 +1,34 @@
 use anyhow::{Context, Result};
-pub fn submit(file_name: &str) -> Result<()> {
-    todo!();
+use scraper::{element_ref::ElementRef, Html, Selector};
+use tokio;
+
+#[tokio::main]
+pub async fn submit(file_name: &str) -> Result<()> {
+    let url = format!("https://atcoder.jp/contests/{}/submit", file_name);
+
+    let client = reqwest::Client::new();
+    let doc = client
+        .get(&url)
+        .send()
+        .await?
+        .error_for_status()?
+        .text()
+        .await?;
+
+    let doc = Html::parse_document(&doc);
+    let csrf_token = doc
+        .select(&Selector::parse("input[name=\"csrf_token\"]").unwrap())
+        .next()
+        .with_context(|| "cannot find csrf_token")?;
+
+    let csrf_token = csrf_token
+        .value()
+        .attr("value")
+        .with_context(|| "cannot find csrf_token")?;
+
+    //println!("{:?}", doc);
+
+    //let url = url.to_string() + "/login";
+
     Ok(())
 }
