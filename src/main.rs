21b130a7d93d336aea7ba9c8712957b29418d0e2
nk86 <n172586k@gmail.com>
2022-07-31 22:17:55 +0900

    add option

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,16 @@
  */
 mod atcoder;
 mod get_testcase;
+mod new;
 mod submit;
 mod test_judge;
-use crate::atcoder::login;
 use crate::get_testcase::get_testcase;
+use crate::new::new;
 use crate::submit::submit;
 use crate::test_judge::test_judge;
 
-use anyhow::{ensure, Context, Result};
+use anyhow::Result;
 use clap::Parser;
-use std::env;
-use std::fs;
 
 #[derive(Parser, Debug)]
 #[clap(
@@ -35,28 +34,28 @@ struct Arg {
     #[clap(short, long)]
     new: Option<String>,
     #[clap(short, long)]
+    extensions: Option<String>,
+    #[clap(short, long)]
     login: Option<String>,
 }
 
 fn main() -> Result<()> {
     let args = Arg::parse();
 
-    //println!("input your user_name!");
-    //let mut user_name = String::new();
-    //std::io::stdin().read_line(&mut user_name).ok();
-    //println!("input your password.");
-    //let mut password = String::new();
-    //std::io::stdin().read_line(&mut password).ok();
-    login();
+    let extensions = if let Some(k) = args.extensions {
+        ".".to_string() + &k
+    } else {
+        ".rs".to_string()
+    };
 
     if let Some(file_name) = args.new {
-        get_testcase(&file_name)?;
+        new(&file_name, &extensions)?;
     } else if let Some(file_name) = args.test {
+        get_testcase(&file_name)?;
         test_judge(&file_name)?;
     } else if let Some(file_name) = args.submit {
         submit(&file_name)?;
     }
 
     Ok(())
-    //println!("{:?}", args);
 }
