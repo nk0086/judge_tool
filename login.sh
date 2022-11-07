ba2a96b2dddd2a688a96049c7ad3780b8361b65e
nk0086 <n172586k@gmail.com>
2022-11-07 11:23:24 +0900

    added the function of login

--- login.sh
+++ login.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# get args
 user=$1
 pass=$2
 
-TOKEN=`curl -sS -c cookie https://atcoder.jp/login | grep -oP 'csrf_token" value=\"(.+=)\"' | sed -r 's/csrf_token\" value=\"(.+=)\"/\1/g' | sed -e 's/+/\+/g' | awk NR==1`
-curl -sS -b cookie https://atcoder.jp/login -X POST -F "csrf_token=${TOKEN}" -F "username=${user}" -F "password=${pass}"
-
+TOKEN=`curl -c cookie https://atcoder.jp/login | grep -oP 'csrf_token" value=\"(.+=)\"' | sed -r 's/csrf_token\" value=\"(.+=)\"/\1/g' | sed -e 's/+/\+/g' | awk NR==1`
+curl -i -b cookie https://atcoder.jp/login -X POST -F "csrf_token=${TOKEN}" -F "username=${user}" -F "password=${pass}"
--- submit.sh
+++ submit.sh
@@ -2,7 +2,7 @@
 contest_id=$1
 problem_id=$2
 language=$3
+file=/home/nkpr/Desktop/rust/judge_tool/abc275_a.rs
 
-TOKEN=`curl -sS -b cookie https://atcoder.jp/login | grep -oP 'csrf_token" value=\"(.+=)\"' | sed -r 's/csrf_token\" value=\"(.+=)\"/\1/g' | sed -e 's/+/\+/g' | awk NR==1`
-curl -sS -b cookie https://atcoder.jp/contests/${contest_id}/submit -X POST -F "TaskScreenName=${problem_id}" -F "LanguageId=4050" -F "sourceCode=</home/nkpr/Desktop/rust/judge_tool/${problem_id}.${language}" -F "csrf_token=${TOKEN}"
-
+TOKEN=`curl -b cookie https://atcoder.jp/login | grep -oP 'csrf_token" value=\"(.+=)\"' | sed -r 's/csrf_token\" value=\"(.+=)\"/\1/g' | sed -e 's/+/\+/g' | awk NR==1`
+curl -i -b cookie https://atcoder.jp/contests/${contest_id}/submit -X POST -F "data.TaskScreenName=${problem_id}" -F "data.LanguageId=4050" -F "sourceCode=@${file}" -F "csrf_token=${TOKEN}"
